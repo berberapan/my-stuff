7073b9ae454a4ceacec9ae455dc286ec7786d2c9
Josef <josef@boukdir.se>
2024-08-29 09:55:18 +0200

    simple route and handler added for home page

---
+++ cmd/web/helpers.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+		trace  = string(debug.Stack())
+	)
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string) {
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, r, err)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+
+	err := ts.ExecuteTemplate(buf, "base", nil)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+
+	w.WriteHeader(status)
+	buf.WriteTo(w)
+}
