f0171be4b4b122c153a3afd98da358bc77eb20bc
Josef <josef@boukdir.se>
2024-08-29 11:27:24 +0200

    web stuff

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,9 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/berberapan/my-stuff/ui"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	mux.HandleFunc("GET /{$}", app.home)
 
